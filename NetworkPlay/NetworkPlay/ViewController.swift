aa5d28517d162f7426b440351e53c200af794727
ashraful-chowdhury <mohammad.ashraful@monstar-lab.com>
2022-06-12 10:19:11 +0600

    feat: added simple  post request

--- NetworkPlay/NetworkPlay/ViewController.swift
+++ NetworkPlay/NetworkPlay/ViewController.swift
@@ -9,6 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBAction func postDataButton(_ sender: Any) {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/posts")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let json : [String : Any] = [
+            "title": "Ash",
+            "body": "Learning iOS",
+            "userId": 1
+        ]
+        
+        let jsonData = try! JSONSerialization.data(withJSONObject: json, options: [])
+        
+        let task = URLSession.shared.uploadTask(with: request, from: jsonData) {data, response, error in
+            if let httpResponse = response as? HTTPURLResponse {
+                print(httpResponse.statusCode)
+            }
+            
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print(dataString)
+            }
+        }
+        
+        task.resume()
+    }
+    
     @IBAction func fetchDataButton(_ sender: Any) {
         let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
         let task = URLSession.shared.dataTask(with: url) {data, response, error in
